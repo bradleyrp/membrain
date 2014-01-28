96a37d2bd665fa3685f2866c86a351bc51741e4b
David R. Slochower <slochower@gmail.com>
2014-01-28 12:39:20 -0500

    Adding a comparison graph.

--- script-plot-msd.py
+++ script-plot-msd.py
@@ -3,21 +3,31 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from numpy import *
+import brewer2mpl
+clrs = brewer2mpl.get_map('Set1', 'qualitative', 5).mpl_colors
 
-lines = []
-fp = open('msd-v532.xvg','r')
-for line in fp:
-	if line[0] != '#' and line[0] != '@':
-		lines.append([float(i) for i in line.split()])
-fp.close()
+analysis_descriptors = ['msd-v530.xvg','msd-v532.xvg']
+analyses = [analysis_descriptors[i] for i in [0,1]]
 
-lines = array(lines)
-tdatall = log10(lines[1:])
-tdat = log10(lines[int(len(tdatall)*.1)+2:int(len(tdatall)*.9)+1])
-[bz,az] = polyfit(tdat[:,0],tdat[:,1],1)
-print bz
-fitted = [bz*i+az for i in tdatall[:,0]]
-plt.plot(tdatall[:,0],tdatall[:,1],'-',c='b',lw=2)
-plt.plot(tdat[:,0],tdat[:,1],'-',c='r',lw=2)
-plt.plot(tdatall[:,0],fitted,'-',c='k',lw=1,alpha=0.5)
+for ad in analyses:
+	lines = []
+	fp = open(ad,'r')
+	for line in fp:
+		if line[0] != '#' and line[0] != '@':
+			lines.append([float(i) for i in line.split()])
+	fp.close()
+
+	lines = array(lines)
+	tdatall = log10(lines[1:])
+	tdat = log10(lines[int(len(tdatall)*.1)+2:int(len(tdatall)*.9)+1])
+	[bz,az] = polyfit(tdat[:,0],tdat[:,1],1)
+	print bz
+	fitted = [bz*i+az for i in tdatall[:,0]]
+	plt.plot(tdatall[:,0],tdatall[:,1],'-',c=clrs[0],lw=2,label=str(ad))
+#	plt.plot(tdat[:,0],tdat[:,1],'-',c='r',lw=2)
+#	plt.plot(tdatall[:,0],fitted,'-',c='k',lw=1,alpha=0.5)
+	plt.xlabel('Time')
+	plt.ylabel('MSD')
+	plt.legend
+
 plt.show()
