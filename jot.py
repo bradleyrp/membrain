359e0ba60d5cf7bd5ce1d6cfca425cbc11b2301e
Ryan Bradley <bradleyrp@gmail.com>
2014-02-20 14:29:48 -0500

    2014.02.20.1430

--- jot.py
+++ jot.py
@@ -16,24 +16,32 @@ from scipy.spatial.distance import *
 
 #---plan
 analysis_descriptors = {
-	'v700-500000-700000-400':
+	'v700-500000-600000-200':
 		{'pklfile':
-		'/structures-broken-transposer-error/pkl.structures.membrane-v700.md.part0009.500000-700000-400.pkl',
-		'label':r'$\mathrm{{EXO70}\ensuremath{\times}2{\small (parallel)}}$'},
+		'pkl.structures.membrane-v700.md.part0009.500000-700000-400.pkl',
+		'label':r'$\mathrm{{EXO70}\ensuremath{\times}2{\small (parallel)}}$','nprots':2,
+		'custom_topogcorr_specs':[[slice(0,23),'coiled coil'],[slice(1224,1226),
+			'PIP2 (1)'],[slice(571,573),'PIP2 (1)']]},
 	'v550-300000-400000-200':
 		{'pklfile':
 		'/structures-broken-transposer-error/pkl.structures.membrane-v550.md.part0006.300000-400000-200.pkl',
-		'label':r'$\mathrm{control}$'},
+		'label':r'$\mathrm{control}$','nprots':0},
 	'v614-120000-220000-200':
 		{'pklfile':'pkl.structures.membrane-v614.s9-lonestar.120000-220000-200.pkl',
-		'label':r'$\mathrm{{ENTH}\ensuremath{\times}4}$'}}
+		'label':r'$\mathrm{{ENTH}\ensuremath{\times}4}$','nprots':4}}
 analysis_names = ['v700-500000-700000-400','v614-120000-220000-200','v550-300000-400000-200']
+analysis_names = ['v614-120000-220000-200']
 plot_reord = analysis_names
 dolist = ['phase_std_spec2d']
-dolist = ['test']
+dolist = ['topogcorr_calc']
 
 #---settings
 cmap = mpl.cm.RdBu_r
+
+#---settings, topographic correlate
+makevid = False #---code needs cleaned up if you use this
+nprots = 2
+binw = (10,1)
 
 #---MAIN
 #-------------------------------------------------------------------------------------------------------------
@@ -112,54 +120,114 @@ if 'phase_std_spec2d' in dolist:
 #---DEV
 #-------------------------------------------------------------------------------------------------------------
 
+makevid = False #---code needs cleaned up if you use this
+nprots = 2
+binw = (10,1)
+
 def torus_metric(x1,x2,vecs,ndims=2):
 	'''The torus metric i.e. how to measure distance in periodic spaces, quickly.'''
 	raw = array([cdist(x1[:,i:i+1],x2[:,i:i+1]) for i in range(ndims)])
 	pbcd = array([raw[i]-(raw[i]>vecs[i]/2)*vecs[i] for i in range(ndims)]).T
 	return sqrt(sum(pbcd**2,axis=-1)).T
 
-if 'test' in dolist and 0:
-	nbins = 41
-	hist_ranges = ((0,400),(-20,20))
-	mset = msets[0]
-	dat_dist_height = []
-	histdat = []
-	#---make up 2d histograms
-	for fr in range(len(mset.surf)):
-		print fr
-		protpts = mset.protein[fr][:,0:2]
-		surfpts = mset.unzipgrid(mset.surf[fr],vecs=mset.vecs[0])
-		#cd = torus_metric(protpts,surfpts,mset.vecs[fr])
-		dat_dist_height.append(array([np.min(cd,axis=0),surfpts[:,2]]).T)
-		H, xedges, yedges = histogram2d(dat_dist_height[-1][:,0],dat_dist_height[-1][:,1],bins=40,range=hist_ranges)
-		row_sums = H.sum(axis=1)
-		#histdat.append(nan_to_num(array(H/(H.sum(axis=1))[:, numpy.newaxis])))
-		histdat.append(array(H))
-	histdat = array(histdat)
-	#plotmov(histdat,'test-heightcorr',smooth=1,xedges=[int(i) for i in xedges],yedges=[int(i) for i in yedges])
+class TopographyCorrelate():
+	'''Class object for holding analysis of topography correlation data.'''
+	def __init__(self,mset,nprots):
+		self.histdat = []
+		self.nprots = nprots
+		self.prots_nres = shape(mset.protein[0])[0]/nprots
+		#---this class is largely just a wrapper for a list of topographic matrices
+		self.prots_refs = [slice(None,None)]+[slice(i*self.prots_nres,(i+1)*self.prots_nres) 
+			for i in range(nprots)]
+		self.cutoff = min(mean(mset.vecs,axis=0)[:2])
+		#---list of matrices of protein-to-surface point distances
+		self.topogmat = []
+		#---list of histogram matrices of protein-to-surface vs surface height distances
+		self.topoghist = []
+		#---default bin widths
+		self.binw = (10,1)
+		#---post-processed mean correlation maps
+		self.hcorr = []
+		self.notes = []
+	def makevid(self,index=0):
+		'''Makes a video of a single topography histogram, specified by index.'''
+		plotmov(tc.topoghist,'test-heightcorr',smooth=1,
+			xedges=[int(i) for i in xedges],yedges=[int(i) for i in yedges])
+	def addnote(self,notable):
+		self.notes.append(notable)
 
-#plt.imshow(mean(histdat,axis=0).T,interpolation='nearest',origin='lower');plt.show()
+#---topography correlate analysis
+if 'topogcorr_calc' in dolist and 0:
+	for a in analysis_names:
+		for i in analysis_descriptors[a]: vars()[i] = (analysis_descriptors[a])[i]
+		m = analysis_names.index(a)
+		mset = msets[m]
+		tc = TopographyCorrelate(mset,nprots)
+		for i in analysis_descriptors[a]: 
+			tc.addnote([i,str((analysis_descriptors[a])[i])])
+		for protsref in tc.prots_refs:
+			hist_ranges = ((0,ceil(tc.cutoff/100)*100),(-40,40))
+			nbins = [(hist_ranges[i][1]-hist_ranges[i][0])/binw[i] for i in range(2)]
+			topogmat = []
+			topoghist = []
+			for fr in range(len(mset.surf)):
+				print 'subset: '+str(protsref)+', frame: '+str(fr)
+				protpts = mset.protein[fr][protsref,0:2]
+				surfpts = mset.wrappbc(mset.unzipgrid(array(mset.surf[fr]),vecs=mset.vec(0)),
+					vecs=mset.vec(fr),mode='nine')
+				#---Nb torus_metric works here
+				#---Nb that the torus metric works fine for distance
+				#---Nb but the torus metric doesn't replicate points the way we want
+				cd = scipy.spatial.distance.cdist(protpts,surfpts[:,:2])
+				topogmat.append(array([np.min(cd,axis=0),surfpts[:,2]]).T)
+				H, xedges, yedges = histogram2d(topogmat[-1][:,0],topogmat[-1][:,1],
+					bins=nbins,range=hist_ranges)
+				row_sums = H.sum(axis=1)
+				topoghist.append(array(H))
+			#---Normalize, then take the mean, resulting in a much smoother curve
+			histdatnorm = nan_to_num([array(topoghist[i]/(topoghist[i].sum(axis=0))) 
+				for i in range(len(topoghist))])
+			hcorr = mean(histdatnorm,axis=0)
+			tc.topoghist.append(array(topoghist))
+			tc.topogmat.append(array(topoghist))
+			tc.hcorr.append(array(hcorr))
+		tc.xedges = xedges
+		tc.yedges = yedges
+		#---wasteful to save the entire distance matrix, so recompute later if necessary
+		del tc.topogmat
+		special_name = "-".join([i for i in re.match('.+membrane\-.+',
+			(analysis_descriptors[a])['pklfile']).string.split('.') 
+			if re.match('[a-z][0-9]\-.+',i) 
+			or re.match('membrane-v.+',i) 
+			or re.match('[0-9]+\-[0-9]+\-[0-9]',i)])
+		pickledump(tc,'pkl.topogcorr.'+special_name+'.pkl',directory=pickles)
+	
+#---plot the 2D histogram
 if 0:
-	sumh = sum(histdat,axis=0)
-	sumhn = nan_to_num(sumh/(sumh.sum(axis=0)))
-	plt.imshow(sumh.T,interpolation='nearest',origin='lower');plt.show()
-donk = [nan_to_num(histdat[i]/histdat[i].sum(axis=1)) for i in range(len(histdat))]
-donkm = mean(donk,axis=0)
-plt.imshow(donkm.T,interpolation='nearest',origin='lower');plt.show()
-#---TOTAL SHITE
-#-------------------------------------------------------------------------------------------------------------
-'''
-if 0:
-	dat_dist_height = []
-	for fr in range(len(mset.surf)):
-		print fr
-		protpts = mset.protein[fr][:,0:2]
-		surfpts = mset.unzipgrid(mset.surf[fr],vecs=mset.vecs[0])
-		cd = scipy.spatial.distance.cdist(protpts,surfpts[:,:2])
-		dat_dist_height.append(array([np.min(cd,axis=0),surfpts[:,2]]).T)
-if 0:
-	H, xedges, yedges = histogram2d(mean(dat_dist_height,axis=0)[:,0],
-		mean(dat_dist_height,axis=0)[:,1],bins=30,normed=True)
-	plt.imshow(array(H).T,interpolation='nearest',origin='lower')
+	fig = plt.figure()
+	ax = plt.subplot(111)
+	ax.imshow(hcorr.T,interpolation='nearest',origin='lower',cmap=mpl.cm.binary)
+	ax.set_xticks(range(len(xedges))[::4])
+	ax.set_yticks(range(len(yedges))[::4])
+	ax.set_xticklabels(xedges[::4])
+	ax.set_yticklabels(yedges[::4])
 	plt.show()
-'''
+#---plot the topography correlate curves
+if 1:
+	smoothwid = 3
+	fig = plt.figure(figsize=(8,8))
+	ax = plt.subplot(111)
+	for h in range(len(tc.hcorr)):
+		hcorr = tc.hcorr[h]
+		if h == 0:
+			label = 'all'
+		elif h < tc.nprots+1:
+			label = 'monomer '+str(h-1)
+		else:
+			label = None
+		ax.plot(tc.xedges[1:-smoothwid],[mean([tc.yedges[hcorr[j].argmax()] for j in range(i+smoothwid)]) 
+			for i in range(len(hcorr)-smoothwid)],'o-',label=label)
+		ax.legend()
+		ax.grid(True)
+	plt.show()
+	
