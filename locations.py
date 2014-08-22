fc32fead95d7c4bf9a8c0604c470f0c8ea9b9020
David R. Slochower <slochower@gmail.com>
2014-08-22 18:34:09 -0400

    2014.08.22.1834

--- locations.py
+++ locations.py
@@ -5,7 +5,7 @@ location_scripts = {
 	'dark.site':'./locations/locations-settings-rpb-dark',
 	'light.site':'./locations/locations-settings-rpb-light',
 	'dirac':'./locations/locations-settings-dirac',
-	'ds':'./locations/locations-settings-ds',
+	'arctic':'./locations/locations-settings-ds',
 	}
 
 #---MAIN
@@ -17,9 +17,14 @@ not_barebones = False
 if 'location' not in globals() or location == '' or location == None:
 	not_barebones = False
 	for key in location_scripts.keys():
-		if subprocess.check_output(['echo $HOSTNAME'],shell=True).strip('\n') == key:
+		# if subprocess.check_output(['echo $HOSTNAME'],shell=True).strip('\n') == key:
+		# This does not always work when running in certain terminals. Stack says this is better. http://goo.gl/tcNpKc
+		import socket
+		if socket.gethostname() == key:
+			print('status: Loading site-specific ' + key + ' location files.')
 			execfile(location_scripts[key])
 			not_barebones = True
+			break
 
 #---if no locations-settings file is specified, use default parameters
 if not not_barebones:
@@ -29,6 +34,8 @@ if not not_barebones:
 	pickles = os.path.abspath(os.path.expanduser('.'))
 
 #---load locations from the table-of-contents from the parse function defined in membrainrunner.py
+
+# It seems basedir is only defined it not_barebones is False (lines 28, 30)
 if not_barebones: [systems,structures,trajectories] = parse_locations_file(basedir,locations)
 
 #---register the postmortem function in membrainrunner.py with the list of globals
