e5d2c0f1a4081ebb1c9bae6fbc42fddb80ae5859
David R. Slochower <slochower@gmail.com>
2014-08-30 17:25:49 -0400

    2014.09.30.1725

--- script-aamd-ion-lipid-bridge.py
+++ script-aamd-ion-lipid-bridge.py
@@ -22,11 +22,11 @@ if 'batch_override' not in globals():
 		'v511-40000-90000-50',
 		'v533-40000-90000-50',
 		'v534-40000-90000-50',
-		][1:2]
+		][2:3]
 
 	routine = [
 		'calculate',
-	    'plot'
+		'plot'
 		][0:1]
 
 	# This is not going to change; this is all script-aamd-ion-lipid-bridge can do.
@@ -36,7 +36,8 @@ if 'batch_override' not in globals():
 
 	# Binding distance in Angstrom, angle dependence not coded yet.
 	binding_cutoff = 3.0
-	binding_cutoff = 5.0
+
+	fast_pbc = True # I'm not even sure what fast_pbc = False does
 
 
 if type(routine) == str: routine = [routine]
@@ -74,7 +75,9 @@ if 'calculate' in routine:
 			num_ions_bridging = [] # Binding to at least two lipids
 			num_lipids_ion_binding = [] # This array holds a list of the number of lipids all ions are binding
 										# by frame. The range is set by variable k.
-			oxygen_pair_list = []
+			which_oxygens = []
+			which_o_pairs = []
+                        total_oxygens = 0
 
 			# Loop over the lipid and the ion selections
 			for lnum in range(2):
@@ -98,7 +101,7 @@ if 'calculate' in routine:
 					else:
 						lipid_selector = 'resname '+pair[lnum]+' and '+binding_atoms[pair[lnum]]
 					phosphodiester_check = list(mset.universe.selectAtoms('resname ' + ptdins_resname + \
-					                                                      ' and (name O14 or name O13)'))
+																		  ' and (name O14 or name O13)'))
 					if len(phosphodiester_check) == 0:
 						status('status: Atoms O13 and O14 are not in the structure file. You are probably " \
 						"using a structure file from /keylipidatoms/ which does not have them included. You can " \
@@ -127,130 +130,120 @@ if 'calculate' in routine:
 				# Out[19]: [<AtomGroup with 40 atoms>, 0.0]
 				pts1 = array(mset.get_points(frameno,selection_index=0))
 				pts2 = array(mset_ions.get_points(frameno,selection_index=0))
-				if fast_pbc: # Not sure what fast_pbc=False does...
-					# All the binding oxygen coordinates in current frame
-					# There are len(pts1)/num_lipids oxygens per lipid
-					# So when checking for ion distances to two different lipids, make sure they are
-					# more than len(pts1)/num_lipids apart.
-					pts1 = array(mset.get_points(frameno,selection_index=0))
-					# All the ion coordinates in current frame
-					pts2 = array(mset_ions.get_points(frameno,selection_index=0))
-					# Could do pre-filtering if I weren't lazy.
-					distance_x = scipy.spatial.distance.cdist(array([[i]
-						for i in pts1[:,0]]),array([[i] for i in pts2[:,0]]))
-					distance_y = scipy.spatial.distance.cdist(array([[i]
-						for i in pts1[:,1]]),array([[i] for i in pts2[:,1]]))
-					distance_z = scipy.spatial.distance.cdist(array([[i]
-						for i in pts1[:,2]]),array([[i] for i in pts2[:,2]]))
-					unwrapped_x = distance_x-1*(distance_x>vecs[0]/2.)*vecs[0]
-					unwrapped_y = distance_y-1*(distance_y>vecs[1]/2.)*vecs[1]
-					unwrapped_z = distance_z-1*(distance_z>vecs[2]/2.)*vecs[2]
-					distance_matrix = sqrt(unwrapped_x**2+unwrapped_y**2+unwrapped_z**2)
-					# Since we know the number of oxygens per lipid for this analysis, we can now
-					# *quickly* group the distances by lipid instead of writing a wrapper loop to do
-					# by residue analysis
-					points_per_lipid = len(pts1)/num_lipids
-					num_binding_sites = len(pts1)
-					num_ions = len(pts2)
-					# i is ion number (e.g 308), j is atom number for all atoms in all lipids (e.g. 240),
-					ion_to_lipid = [[distance_matrix[:,i][j:j+points_per_lipid] \
-							for j in arange(0,num_binding_sites,points_per_lipid)] \
-					        for i in range(num_ions)]
+				# All the binding oxygen coordinates in current frame
+				# There are len(pts1)/num_lipids oxygens per lipid
+				# So when checking for ion distances to two different lipids, make sure they are
+				# more than len(pts1)/num_lipids apart.
+				pts1 = array(mset.get_points(frameno,selection_index=0))
+				# All the ion coordinates in current frame
+				pts2 = array(mset_ions.get_points(frameno,selection_index=0))
+				# Could do pre-filtering if I weren't lazy.
+				distance_x = scipy.spatial.distance.cdist(array([[i]
+					for i in pts1[:,0]]),array([[i] for i in pts2[:,0]]))
+				distance_y = scipy.spatial.distance.cdist(array([[i]
+					for i in pts1[:,1]]),array([[i] for i in pts2[:,1]]))
+				distance_z = scipy.spatial.distance.cdist(array([[i]
+					for i in pts1[:,2]]),array([[i] for i in pts2[:,2]]))
+				unwrapped_x = distance_x-1*(distance_x>vecs[0]/2.)*vecs[0]
+				unwrapped_y = distance_y-1*(distance_y>vecs[1]/2.)*vecs[1]
+				unwrapped_z = distance_z-1*(distance_z>vecs[2]/2.)*vecs[2]
+				distance_matrix = sqrt(unwrapped_x**2+unwrapped_y**2+unwrapped_z**2)
+				# Since we know the number of oxygens per lipid for this analysis, we can now
+				# *quickly* group the distances by lipid instead of writing a wrapper loop to do
+				# by residue analysis
+				points_per_lipid = len(pts1)/num_lipids
+				num_binding_sites = len(pts1)
+				num_ions = len(pts2)
+				# i is ion number (e.g 308), j is atom number for all atoms in all lipids (e.g. 240),
+				ion_to_lipid = [[distance_matrix[:,i][j:j+points_per_lipid] \
+						for j in arange(0,num_binding_sites,points_per_lipid)] \
+						for i in range(num_ions)]
 
-					# min_ion_to_lipid[ion][:] shows the minimum distance between a given ion and any binding
-					# oxygen on a given lipid
-					min_ion_to_lipid = [[ion_to_lipid[i][j][:].min() \
-		                    for j in range(num_lipids)] \
-                            for i in range(num_ions)]
+				# min_ion_to_lipid[ion][:] shows the minimum distance between a given ion and any binding
+				# oxygen on a given lipid
+				min_ion_to_lipid = [[ion_to_lipid[i][j][:].min() \
+						for j in range(num_lipids)] \
+						for i in range(num_ions)]
 
-					# If we keep track of *where* the minimum is, then we can also keep track of which oxygen
-					# is doing the binding. And if we keep track of both minima, then we know which two oxygens
-					# are being bridged by the ion. This assumes two factors
-					# a) That SelectAtoms() outputs coordinates in the same order as inputs
-					# b) That cdist stores the distance between input i and input j in the ij'th entry in output matrix
+				# If we keep track of *where* the minimum is, then we can also keep track of which oxygen
+				# is doing the binding. And if we keep track of both minima, then we know which two oxygens
+				# are being bridged by the ion. This assumes two factors
+				# a) That SelectAtoms() outputs coordinates in the same order as inputs
+				# b) That cdist stores the distance between input i and input j in the ij'th entry in output matrix
 
-					min_index_and_value = [[min(enumerate(ion_to_lipid[i][j]), key=operator.itemgetter(1)) \
-					                        for j in range(num_lipids)] \
-					                        for i in range(num_ions)]
-
-
-					# Check if a given ion is binding to a single lipid:
-					binding_truth_table = [any([i<binding_cutoff for i in min_ion_to_lipid[j]]) \
-							for j in range(num_ions)]
-					fraction_binding = float(sum(binding_truth_table))/float(len(binding_truth_table))
-
-					bridging_truth_table = [len(where([i<binding_cutoff for i in min_ion_to_lipid[j]])[0]) >= 2 \
-					                  for j in range(num_ions)]
-					fraction_bridging = float(sum(bridging_truth_table))/float(len(bridging_truth_table))
-
-					num_ions_binding.append(sum(binding_truth_table))
-					num_ions_bridging.append(sum(bridging_truth_table))
-					this_frame = [] # This array holds a list of the number of lipids an ion is binding /per frame/
-
-					# !?!?!?!?!!
-					which_oxygens = [] # This array holds a list of which oxygens an ion is binding /per frame/
-					total_oxygens = 0
-
-					# Assumption: no ion is coordinating more than 4 lipids
-					for k in range(4):
-						# The first method works just fine, but does not use the matrix that keeps track of the oxygens
-						# num_lipids_binding = [len(where([i < binding_cutoff for i in min_ion_to_lipid[j]])[0]) == k \
-						#                      for j in range(num_ions)]
-
-						ion_to_lipid_min_dists =  [[ row[1] for row in min_index_and_value[j]] for j in range(num_ions)]
-						num_lipids_binding = [len(where([i < binding_cutoff for i in ion_to_lipid_min_dists[j]])[0])
-						                      == k for j in range(num_ions)]
-						# This array holds the index of the oxygen on a specific lipid (0 to 6 or 8) that is closest to a given ion
-						ion_to_lipid_index_of_min = [[ row[0] for row in min_index_and_value[0]] for j in range(num_ions)]
-						if k == 1:
-							# This is going to be slow because now I'm repeating the where() function, but at least it
-						    # is readable.
-							# This contains the position of the lipids (0 to 40 or so) that fit the binding criterion
-							# for each ion, but some ions will not be binding so many entries will be blank arrays.
-
-							# print 'There should be '+str(len(where(num_lipids_binding)[0]))+' single oxygen-ion ' \
-							#       'interactions in this frame.'
-							total_oxygens += len(where(num_lipids_binding)[0])
-
-							#for i in range(num_ions):
-							#	if num_lipids_binding[i]: # If it is true
-
-							which_ions_binding_single_lipid = where(num_lipids_binding) # Where it is true
-							for i in which_ions_binding_single_lipid[0]:
-								this_ion = ion_to_lipid_min_dists[i]
-								# Find location of minimum
-								min_index = this_ion.index(min(this_ion))
-								this_oxygen = ion_to_lipid_index_of_min[i][min_index]
-								which_oxygens.append(this_oxygen)
-							# 0: OP52 (bound to H52 in PI2P)
-							# 1: OP53
-							# 2: OP54
-							# 3: OP42
-							# 4: OP43
-							# 5: OP44
-							# 6: O13
-							# 7: O14
-							D = {'OP52':0, 'OP53':1, 'OP54':2, 'OP42':3,'OP43':4, 'OP44':5, 'O13':6, 'O14':7}
-							if interactive == 1:
-								oxygen_count = dict((i,which_oxygens.count(i)) for i in which_oxygens)
-								plt.bar([i for i in range(len(oxygen_count))],[oxygen_count[i] for i in range(len(oxygen_count))], align='center')
-
-								# This is wrong because D.keys() returns the items UNSORTED!
-								plt.xticks(range(len(D)), D.keys())
+				min_index_and_value = [[min(enumerate(ion_to_lipid[i][j]), key=operator.itemgetter(1)) \
+										for j in range(num_lipids)] \
+										for i in range(num_ions)]
 
 
-								plt.title('Distribution of oxygens binding to calcium ions which only bind one oxygen')
-								plt.show()
+				# Check if a given ion is binding to a single lipid:
+				binding_truth_table = [any([i<binding_cutoff for i in min_ion_to_lipid[j]]) \
+						for j in range(num_ions)]
+				fraction_binding = float(sum(binding_truth_table))/float(len(binding_truth_table))
+				bridging_truth_table = [len(where([i<binding_cutoff for i in min_ion_to_lipid[j]])[0]) >= 2 \
+								  for j in range(num_ions)]
+				fraction_bridging = float(sum(bridging_truth_table))/float(len(bridging_truth_table))
+
+				# Need to normalize this by the number of PI2P that /could/ be bridged, by finding
+				# the closest distance between oxygens of two adjacent residues and comparing with binding_cuytoff.
+
+				num_ions_binding.append(sum(binding_truth_table))
+				num_ions_bridging.append(sum(bridging_truth_table))
+				this_frame = [] # This array holds a list of the number of lipids an ion is binding /per frame/
+				which_oxygens_frame = [] # This array holds a list of which oxygens an ion is binding /per frame/
+				which_o_pairs_frame = [] # This array holds a list of oxygen pairs an ion is binding /per frame/
+
+				# Assumption: no ion is coordinating more than 4 lipids
+				for k in range(4):
+					# The first method works just fine, but does not use the matrix that keeps track of the oxygens
+					# num_lipids_binding = [len(where([i < binding_cutoff for i in min_ion_to_lipid[j]])[0]) == k \
+					#                      for j in range(num_ions)]
+					ion_to_lipid_min_dists =  [[ row[1] for row in min_index_and_value[j]] for j in range(num_ions)]
+					num_lipids_binding = [len(where([i < binding_cutoff for i in ion_to_lipid_min_dists[j]])[0])
+										  == k for j in range(num_ions)]
+					# This array holds the index of the oxygen on a specific lipid (0 to 6 or 8) that is closest to a given ion
+					ion_to_lipid_index_of_min = [[ row[0] for row in min_index_and_value[0]] for j in range(num_ions)]
+					if k == 1:
+						# print 'There should be '+str(len(where(num_lipids_binding)[0]))+' single oxygen-ion ' \
+						#	   'interactions in this frame.'
+						total_oxygens += len(where(num_lipids_binding)[0])
+						which_ions_binding_single_lipid = where(num_lipids_binding) # Where it is true
+						for i in which_ions_binding_single_lipid[0]:
+							this_ion = ion_to_lipid_min_dists[i]
+							# Find location of minimum
+							min_index = this_ion.index(min(this_ion))
+							this_oxygen = ion_to_lipid_index_of_min[i][min_index]
+							which_oxygens_frame.append(this_oxygen)
+					elif k == 2:
+						# Here we want to keep track of *pairs* not just single atoms.
+                                                total_oxygens += len(where(num_lipids_binding)[0])
+                                                which_ions_binding_two_lipids = where(num_lipids_binding)
+                                                for i in which_ions_binding_two_lipids[0]:
+                                                        this_ion = ion_to_lipid_min_dists[i]
+                                                        min_indices = argsort(ion_to_lipid_min_dists[i])[0:2]
+                                                        these_oxygens = ion_to_lipid_index_of_min[i][min_indices[0]],ion_to_lipid_index_of_min[i][min_indices[1]]
+                                                        which_o_pairs_frame.append(these_oxygens)
+                                this_frame.append(sum(num_lipids_binding))
+			        num_lipids_ion_binding.append(this_frame)
+	        	        which_oxygens.append(which_oxygens_frame)
+                                which_o_pairs.append(which_o_pairs_frame)
 
 
-						# num_lipids_ion_binding.append([k,sum(num_lipids_binding)])
-						# binding = [k, sum(num_lipids_binding)]
-						# this_frame.append(binding)
-						# Can infer "k" from position in matrix.
-						this_frame.append(sum(num_lipids_binding))
-				num_lipids_ion_binding.append(this_frame)
-				# tmp = item for sublist in num_lipids_ion_binding for item in sublist]
-				# tmp2 = list(zip(*tmp)[0]) # Number of ions, per frame, binding 0 lipids
+
+			# Using a dictionary would be nice, but D.keys() returns items in a random unsorted way.
+			# D = {'OP52':0, 'OP53':1, 'OP54':2, 'OP42':3,'OP43':4, 'OP44':5, 'O13':6, 'O14':7}
+			D = ['OP52','OP53','OP54','OP42','OP43','OP44','O13','O14']
+                        interactive = 1
+			if interactive == 1:
+				checktime()
+				print 'Counting oxygens'
+				big_list = [item for sublist in which_oxygens for item in sublist]
+				# This really ought to be replaced because it's so slow...
+				oxygen_count = dict((i,big_list.count(i)) for i in big_list)
+				plt.bar([i for i in range(len(oxygen_count))],[float(oxygen_count[i])/len(big_list) for i in range(len(oxygen_count))], align='center')
+				plt.xticks(range(len(D)), [D[i] for i in range(len(D))])
+				plt.title('Distribution of oxygens binding to calcium ions which only bind one oxygen')
+				plt.show()
 
 			# The following bits help figure out what's going wrong when the results are donkey bonkers...
 			debug = 0
@@ -258,10 +251,10 @@ if 'calculate' in routine:
 				import itertools as it
 				d = [[max([y - x for x, y in it.combinations(ion_to_lipid[i][j][:], 2)]) \
 					 for j in range(num_lipids)] \
-				     for i in range(num_ions)]
+					 for i in range(num_ions)]
 				print "The maximum difference between the distance of all oxygen atoms of a specific lipid " \
-				      "to given ion can be checked by accessing d[ion][lipid] and this difference should " \
-				      "be relatively small (probably less than 10 A)."
+					  "to given ion can be checked by accessing d[ion][lipid] and this difference should " \
+					  "be relatively small (probably less than 10 A)."
 				# For each ion, how far away is nearest lipid binding oxygen?
 				# distance_matrix.min(axis=0)
 				# For each ion, how far away are the /two/ nearest lipid binding oxygens?
@@ -291,7 +284,7 @@ if 'calculate' in routine:
 		pickledump(mset.store[0],'pkl.bridge.'+'-'.join(pairnames)+'.'+specname_mod+'.pkl',directory=pickles)
 		checktime()
 		del mset
-		#del mset_ions
+		del mset_ions
 		del result_data
 
 if 'plot' in routine:
@@ -323,7 +316,7 @@ if 'plot' in routine:
 			all_cutoff_bins.append(cutoff_bins)
 		consensus_cutoff_bins = min(all_cutoff_bins)
 		maxpeak = 0
- 		fig = plt.figure(figsize=(8,6))
+		fig = plt.figure(figsize=(8,6))
 		gs = mpl.gridspec.GridSpec(1,1)
 		ax = fig.add_subplot(gs[0])
 		plotted_objects = 0
